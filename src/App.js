22223d1f1cc3ada83ea5247f79f4a89b8c8327b0
Lviathannn <valorant.leviathan@gmail.com>
2022-10-09 10:16:03 +0700

    Convert HTML to React JS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-function App() {
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Details from "./pages/Details";
+import Cart from "./pages/Cart";
+import Congratulation from "./pages/Congratulation";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
    return (
-      <div className="App">
-         <h1 className="text-indigo-500 text-5xl font-extrabold">
-            Hello World
-         </h1>
-      </div>
+      <>
+         <BrowserRouter>
+            <Routes>
+               <Route path="/" element={<HomePage />} />
+               <Route path="/categories/:idc" element={<Details />} />
+               <Route path="/cart" element={<Cart />} />
+               <Route path="/congratulation" element={<Congratulation />} />
+               <Route path="*" element={<NotFound />} />
+            </Routes>
+         </BrowserRouter>
+      </>
    );
 }
-
-export default App;
